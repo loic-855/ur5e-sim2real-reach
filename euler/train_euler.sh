e49ccfe0949da47480f3495c2186e9f760cdaf4d
Loic Renevey <lrenevey@eu-login-11.euler.ethz.ch>
2026-03-03 13:01:39 +0100

    latest change in train_euler

--- euler/train_euler.sh
+++ euler/train_euler.sh
@@ -5,14 +5,14 @@
 #SBATCH -n 1
 #SBATCH --cpus-per-task=2
 #SBATCH --gpus=rtx_4090:1
-#SBATCH --time=01:00:00
+#SBATCH --time=00:45:00
 #SBATCH --mem-per-cpu=8000
-#SBATCH --job-name="WWSim-Pose-Orientation-Sim2Real-Direct-v2"
+#SBATCH --job-name="WWSim-Pose-Orientation-Sim2Real-Direct-v3"
 #SBATCH --output=logs/train_%j.out
 #SBATCH --error=logs/train_%j.err
 
 # --- CONFIGURATION ---
-TASK_NAME="WWSim-Pose-Orientation-Sim2Real-Direct-v2"
+TASK_NAME="WWSim-Pose-Orientation-Sim2Real-Direct-v3"
 # UPDATE THIS PATH to where you uploaded your .sif file
 SIF_PATH="/cluster/scratch/$USER/isaac_euler_salziegl.sif"
 
@@ -75,14 +75,23 @@ apptainer exec --nv \
         # 2. Run Training
         echo 'Starting Training...'
         /isaac-sim/python.sh /workspace/isaaclab/$PROJECT_NAME/scripts/rsl_rl/train.py \
-            --task $TASK_NAME\
+            --task=$TASK_NAME \
             --headless \
-            --max_iter 5000 \
-            agent.algorithm.entropy_coef=0.04 \
-            agent.algorithm.desired_kl=0.013 \
-            env.env_reset=0.0 \
-            env.progressive_reset_steps=40000 \
-            env.contact_penalty_scale=-0.005
+            agent.max_iterations=2500 \
+            env.stability_reward_scale=0.3 \
+            env.action_penalty_scale=-0.01 \
+            env.velocity_action_penalty_scale=-0.05 \
+            env.velocity_penalty_scale=-0.01 \
+            env.position_exp_scale=0.05 \
+            env.orientation_exp_scale=0.2 \
+            env.action_scale=2.0 \
+            env.pos_threshold=0.05 \
+            env.required_frames=15 \
+            env.domain_rand.enabled=True \
+            env.domain_rand.action_delay_range=[0,2] \
+            env.domain_rand.obs_delay_range=[0,1] \
+            agent.experiment_name=sim2real_v3_rand_delay \
+            agent.wandb_project=sim2real_v3_rand_delay 
     "
 
 # Cleanup Cache
