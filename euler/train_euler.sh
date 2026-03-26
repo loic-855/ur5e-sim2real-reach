0d6ef7994ad51b7435cb810f5d6ce244587d887b
Loïc <lrenevey@student.ethz.ch>
2026-03-26 13:21:04 +0100

    new euler train

--- euler/train_euler.sh
+++ euler/train_euler.sh
@@ -88,7 +88,8 @@ apptainer exec --nv \
             env.domain_rand.action_delay_range=[0,0] \
             env.domain_rand.obs_delay_range=[0,0] \
             env.goal_sampling_random_ratio=1.0 \
-            env.goal_timeout_s=10.0
+            env.goal_timeout_s=10.0 \
+            env.goal_height=[0.1,0.6]
     "
 
 # Cleanup Cache
